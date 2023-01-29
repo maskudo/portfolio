ec083243b64ea49b570b43b4cc8282284a61d782
maskudo <manujkarki101@gmail.com>
2023-01-29 21:48:57 +0545

    setup for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import NavBar from './components/NavBar';
 
 function App() {
-  return <div className='app'>hello world</div>;
+  return (
+    <div className='app'>
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
